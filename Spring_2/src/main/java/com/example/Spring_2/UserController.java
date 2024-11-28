b74bd21335e42af3597122300ab291a95b9cf32e
MU <m.iouriev@gmail.com>
2024-11-28 17:20:15 +0700

    task 2

---
+++ Spring_2/src/main/java/com/example/Spring_2/UserController.java
@@ -0,0 +1,44 @@
+package com.example.Spring_2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+
+
+        model.addAttribute("users", users);
+        return "user-list";
+        //return "home.html";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable("id") int id){
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+}
